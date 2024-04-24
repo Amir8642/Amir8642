93600714d5eaa5e02b432b07a69a414ab1fad445
Amir8642 <amirvahobov707@gmail.com>
2024-04-24 18:37:43 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// console.log(1 + 2);
+// console.log(1 - 2);
+// console.log(1 * 2);
+// console.log(1 / 2);
+
+// console.log(2 ** 4);
+// console.log(4 % 3);
+
+
+// let a = '45'
+// let b = 45
+
+// console.log(b + a);
+
+
+// console.log(2 == 2);
+// console.log(2 == 32);
+
+
+// const a = Math.round(45.8)
+// const b = Math.floor(45.9)
+// const c = Math.ceil(45.1)
+// const d = Math.pow(2, 5)
+// const e = Math.random() * 100
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+
+// const age = prompt('Введите возраст')
+// const isMarried = confirm('Вам есть 18 лет)')
+// const abc = alert('Продолжить?')
+// console.log(age);
+// console.log(isMarried);
+// console.log(abc);
+
+
